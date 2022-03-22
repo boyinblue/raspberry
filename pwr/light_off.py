405b1a81d567baec8c4851ca088dc32807f044d3
boyinblue <boyinblue@naver.com>
2022-03-23 06:00:04 +0900

    light off pin map error fix

--- pwr/light_off.py
+++ pwr/light_off.py
@@ -3,8 +3,8 @@ import RPi.GPIO as GPIO
 def light_off():
     #GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(24, GPIO.OUT)
-    GPIO.output(24, True) #Low Active
+    GPIO.setup(23, GPIO.OUT)
+    GPIO.output(23, True) #Low Active
 
 if __name__ == '__main__':
     light_off()
