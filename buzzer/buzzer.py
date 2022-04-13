a15da191ba0e520383696b22b14e8144ce13e6db
boyinblue <boyinblue@naver.com>
2022-04-13 15:31:43 +0900

    update

--- buzzer/buzzer.py
+++ buzzer/buzzer.py
@@ -1,21 +1,55 @@
 import RPi.GPIO as GPIO
 import sys
+import time
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(25, GPIO.OUT)
+pin_num = None
+
+def buzzer_init(buzzer_pin_num = 25):
+    global pin_num
+
+    pin_num = buzzer_pin_num
+
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin_num, GPIO.OUT)
+    GPIO.output(pin_num, False)
 
 def buzzer_on():
-    GPIO.output(25, True)
+    if not pin_num:
+        print("pin number not defined")
+        return
+    GPIO.output(pin_num, True)
 
 def buzzer_off():
-    GPIO.output(25, False)
+    if not pin_num:
+        print("pin number not defined")
+        return
+    GPIO.output(pin_num, False)
+
+def buzzer_beep(duration=0.1, times=1):
+    if not pin_num:
+        print("pin number not defined")
+        return
+    for i in range(int(times)):
+        buzzer_on()
+        time.sleep(float(duration))
+        buzzer_off()
+        time.sleep(float(duration))
 
 def main():
+    buzzer_init(25)
+
     if sys.argv[1] == "on":
         buzzer_on()
     elif sys.argv[1] == "off":
         buzzer_off()
+    elif sys.argv[1] == "beep":
+        if len(sys.argv) > 3:
+            buzzer_beep(sys.argv[2], sys.argv[3])
+        elif len(sys.argv) > 2:
+            buzzer_beep(sys.argv[2])
+        else:
+            buzzer_beep()
 
 if __name__ == '__main__':
     main()
