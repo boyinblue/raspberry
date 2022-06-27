5f901a90ab2a1209ee565ec247668c7364b564ca
parksejin <parksejin@parksejin-desktop>
2022-06-28 07:01:49 +0900

    update

--- get_rtc/get_rtc.sh
+++ get_rtc/get_rtc.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-#current_time=$(ssh parksejin@15.26.208.40 "date '+%Y-%m-%d %H:%M:%S'")
-current_time=$(ssh parksejin@175.118.12.219 "date '+%Y-%m-%d %H:%M:%S'")
-#current_time=$(ssh parksejin@15.38.179.105 "date '+%Y-%m-%d %H:%M:%S'")
-timedatectl set-time "$current_time"
+#current_time=$(ssh parksejin@15.40.187.106 "date '+%Y-%m-%d %H:%M:%S'")
+#current_time=$(ssh parksejin@175.118.12.219 "date '+%Y-%m-%d %H:%M:%S'")
+
+ips=("15.26.228.11" "www.dhqhrtnwl.shop")
+
+function get_rtc
+{
+  ip="${1}"
+
+  echo "ping -c 1 $ip"
+  ping -c 1 "${ip}"
+
+  if [ "$?" == "0" ]; then
+    current_time=$(ssh parksejin@${ip} "date '+%Y-%m-%d %H:%M:%S'")
+    timedatectl set-time "$current_time"
+    echo "RTC setting completed"
+    exit 0
+  fi
+}
+
+for ip in ${ips[@]}
+do
+  echo "Checking IP ${ip}"
+  get_rtc "${ip}"
+done
